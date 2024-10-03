5282e6cc6ad0a2d1401b495b465983efaad80a74
ypww <rororo3434@yahoo.co.jp>
2024-10-03 22:38:23 +0900

    AC

---
+++ atcoder/virtual/att/abc179_f.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+template <class T> using priority_queue_rev = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i, a, b) for(ll i=a; i<b; i++)
+#define rrep(i, a, b) for(ll i=a; i>=b; i--)
+#define all(a) (a).begin(), (a).end()
+#define smod(n, m) ((((n) % (m)) + (m)) % (m)) // 非負mod
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+inline int popcount(int n) { return __builtin_popcount(n); } // 2進数で表した場合に立ってるビット数がいくつか
+inline int popcount(ll n) { return __builtin_popcountll(n); }
+inline int ctz(int n) { return n != 0 ? __builtin_ctz(n) : -1; } // 2進数で表した場合に 1 の位からいくつ 0 が連なっているか
+inline int ctz(ll n) { return n != 0 ? __builtin_ctzll(n) : -1; }
+inline int clz(int n) { return n != 0 ? (31 - __builtin_clz(n)) : -1; } // 2進数で表した場合に左側にいくつ 0 を埋める必要があるか
+inline int clz(ll n) { return n != 0 ? (63 - __builtin_clzll(n)) : -1; }
+
+const double PI = 3.141592653589793;
+const vector<int> DX = { 1, 0, -1, 0 };
+const vector<int> DY = { 0, 1, 0, -1 };
+ll coordinate(ll h, ll w, ll W){ return h*W + w; } // 二次元座標を一次元座標に変換
+ll dist2(ll x1, ll y1, ll x2, ll y2){ return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2); }
+
+#define endl "\n" // インタラクティブの時はコメントアウトする
+
+using S = long long;
+using F = long long;
+
+const S INF = 8e18;
+const F ID = 8e18;
+
+S op(S a, S b){ return std::min(a, b); }
+S e(){ return INF; }
+S mapping(F f, S x){ return std::min(f, x); }
+F composition(F f, F g){ return std::min(f, g); }
+F id(){ return ID; }
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    // cout << fixed << setprecision(18);
+
+    ll n, q;
+    cin >> n >> q;
+
+    std::vector<S> v(n, n-1);
+    atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> seg_xmin(v), seg_ymin(v);
+
+    ll ans = (n-2) * (n-2);
+    while(q--){
+        ll t, x;
+        cin >> t >> x;
+        x--;
+        if (t == 1){
+            ll r = seg_ymin.get(x);
+            ans -= r-1;
+            seg_xmin.apply(1, r, x);
+        }else{
+            ll r = seg_xmin.get(x);
+            ans -= r-1;
+            seg_ymin.apply(1, r, x);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
